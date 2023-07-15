45de78cd10da71644272b96a76086884687ff7b0
Trần Trung Dũng <dungtran@MacBook-Pro-cua-Tran.local>
2023-07-15 15:16:35 +0700

    minor

--- front-end/src/Constants.js
+++ front-end/src/Constants.js
@@ -1,1 +1,3 @@
-export const PROXY="https://volcash-backend.wovodat.org"
+export const PROXY = "https://volcash-backend.wovodat.org"
+export const ORGINAL_ZIP = "particles.zip"
+export const COMPRESSED_ZIP = "compressedImages.zip"
